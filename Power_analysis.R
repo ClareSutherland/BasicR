4ed4aac19136e3338c4e5107548413f9225e18d3
Clare Sutherland <38910848+ClareSutherland@users.noreply.github.com>
2019-02-25 11:30:01 +0800

    Add files via upload

--- Power_analysis.R
+++ Power_analysis.R
@@ -1,15 +1,23 @@
-#need to load package since it's not one of R's inbuilt packages
+# ------------------------------------------------------------------------------
+# Author: Clare Sutherland
+# Date: 21.02.19
+#
+# Script for power analysis
+# -------|---------|---------|---------|---------|---------|---------|---------|
+
+# need to load the power analysis package since it's not one of R's inbuilt packages
 library(pwr)
 
-#this is a power analysis for a between subjects ttest, to decide sample size
-#it runs for the number of subjects, but you can set this instead with n = ,
-#and remove another aspect to calculate this value instead e.g. effect size
-#type can be changed to 'paired' or 'one.sample'
+# this is an apriori power analysis to decide sample size for a between subjects t-test
+# the test calculates the number of subjects needed for a given power, effect size and sample size
+# you can also run post-hoc power calculations by setting number of subjects instead with n = [whatever you want]
+# then leave blank whatever aspect you want to calculate (e.g. observed power or effect size)
+# note that the t-test type can be changed to 'paired' or 'one.sample'
 pwr.t.test(sig.level = 0.05, power = 0.80, d = 0.2, type = 'two.sample')
 
-#this is a power analysis for a correlation, to decide sample size
-#it runs for the number of subjects, but you can set this instead with n = ,
-#and remove another aspect to calculate this value instead e.g. effect size
+# this is an apriori power analysis to decide sample size for a correlation
+# the test calculates the number of subjects needed for a given power, effect size and sample size
+# again, you can set the N and then leave other aspects blank to calculate these instead. Have a play!
 pwr.r.test(sig.level = 0.05, power = 0.80, r = 0.5)
 
-#google "PWR help" for more info or type ??pwr into the RStudio console
+# for help, google "PWR help" for more info or type ??pwr into the RStudio console for help
