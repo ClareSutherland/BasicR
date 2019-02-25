c42b351afe8e1e64d2468de0222a68172eafff25
Clare Sutherland <38910848+ClareSutherland@users.noreply.github.com>
2019-02-25 11:29:34 +0800

    Add files via upload

--- Loading_data.R
+++ Loading_data.R
@@ -1,11 +1,21 @@
-#Get the current location of the R script
+# ------------------------------------------------------------------------------
+# Author: Clare Sutherland
+# Date: 21.02.19
+#
+# Script for loading data
+# -------|---------|---------|---------|---------|---------|---------|---------|
+
+# Tell R where to find your file (i.e. set working directory to current folder location)
+dir.name <- dirname(rstudioapi::getActiveDocumentContext()$path)
+setwd(dir.name)
+# Show the current location to check it's OK
 getwd()
 
-#Change the current location so that it's in the same folder as the data
-#NB replace the text in the quotation marks with your filepath
-setwd("/Users/00089951 1/Dropbox/R workshop/RWorkshop1")
 
-#If you don't know the filepath, type file.choose() in the console to print the filepath into R
+  #NB you can also manually set the current folder location. In this case, 
+#replace the text in the quotation marks with your filepath.
+#If you don't know the filepath, you can type file.choose() in the console to print the filepath into R
+#setwd("/Users/00089951/Dropbox/RTWs/R workshop materials 2/RWorkshop1")
 
 #load a csv file called 'DuckGroups' into R
 #csv file has a top row which holds the variable names (that's why headers = TRUE)
